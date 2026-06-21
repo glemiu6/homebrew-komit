81f92a4a4dfc8e55dd8b3d90a191ca840086268b
glemiu6 <digorivlad59@gmail.com>
2026-06-21 22:50:21 +0100

    feat: add komit formula [main] - Add AI-powered git commit message generator using local LLMs

---
+++ Formula/komit.rb
@@ -0,0 +1,17 @@
+class Komit < Formula
+  desc "AI-powered git commit message generator using local LLMs"
+  homepage "https://github.com/glemiu6/komit"
+  url "https://github.com/glemiu6/komit/archive/refs/tags/v0.4.4.tar.gz"
+  sha256 "YOUR_SHA256_HERE"
+  license "Apache-2.0"
+
+  depends_on "python@3.13"
+
+  def install
+    system "pip3", "install", *std_pip_args, "."
+  end
+
+  test do
+    system "#{bin}/komit", "--version"
+  end
+end
